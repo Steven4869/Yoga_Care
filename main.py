8ec8329948de246c3f546dd6341ddbe090ddf847
Shivam Sinha <shivamsinha@Shivams-MacBook-Pro.local>
2022-02-12 22:07:19 +0530

    Yoga Care files added

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import pymysql
+pymysql.install_as_MySQLdb()
+from flask_mysqldb import MySQL
+import yaml
+
+
+
+app = Flask(__name__)
+
+db = yaml.full_load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/positions")
+def positions():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM data")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('positions.html', userDetails=userDetails)
+
+
+if (__name__ == '__main__'):
+    app.run(debug=True)
